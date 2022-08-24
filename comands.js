704603191dccf5f13a1598c2404956228df23a3c
Christian Vargas <cristhiandavid.vargas@vmlyr.com>
2022-08-24 09:07:26 -0500

    probando

---
+++ comands.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ * ==========================================================
+ * ========== SISTEMA DE ESPACIOS ==========
+ * Sistema configurable de espacios y tamaños, es algo que se
+ * puede configurar.
+ * 
+ * buscar spacing en el archivo tailwind.config.full.js
+ * 
+ * PROPIEDADES => estas propiedades pueden controlar las dimensiones
+ * o espacios
+ * - Height; - Width; - Margin; - Padding
+ * 
+ * docs: https://platzi.com/clases/1822-tailwind-css-2020/26606-dimensiones-y-espacios/
+ *
+ * 
+ * ==========================================================
+ * ========== TIPOGRAFIA ==========
+ * Tipo de letra
+ * docs: https://tailwindcss.com/docs/font-family
+ * 
+ * Tamaño
+ * docs: https://tailwindcss.com/docs/font-size
+ * 
+ * Cursiva y Negrita
+ * docs: https://tailwindcss.com/docs/font-style
+ * 
+ * Peso
+ * docs: https://tailwindcss.com/docs/font-weight
+ * 
+ * 
+ * === ESPACIO DE LAS LETRAS ===
+ * 
+ * - TRACKING => espaciado entre letras
+ *   Docs: https://tailwindcss.com/docs/line-height
+ * 
+ * - LEADING => espaciamiento entre lineas
+ *   Docs: https://tailwindcss.com/docs/line-height
+ * 
+ * - TEXT ALIGN
+ *   Docs: https://tailwindcss.com/docs/text-align
+ * 
+ * - TEXT DECORATION
+ *   Docs: https://tailwindcss.com/docs/text-decoration
+ * 
+ * - TEXT TRANSFORM
+ *   Docs: https://tailwindcss.com/docs/text-transform
+ * 
+ * ==========================================================
+ * ========== DISPLAY ==========
+ * CAJA DE PRESENTACION
+ * - Block; - Inline-block; - Inline; - Hidden; - Flex
+ * 
+ * https://tailwindcss.com/docs/display#hidden
+ * 
+ * 
+ * ===== FLEX =====
+ * - Flex direction;   - Align items
+ * - Justify Content   - Order
+ * 
+ * docs: https://tailwindcss.com/docs/flex
+ * */
