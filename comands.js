30b171e916fc44ebb690c55213d681ff4204cb42
Christian Vargas <cristhiandavid.vargas@vmlyr.com>
2022-08-24 18:47:06 -0500

    reto taildwind

--- comands.js
+++ comands.js
@@ -49,14 +49,84 @@
  * ==========================================================
  * ========== DISPLAY ==========
  * CAJA DE PRESENTACION
+ * 
  * - Block; - Inline-block; - Inline; - Hidden; - Flex
  * 
  * https://tailwindcss.com/docs/display#hidden
  * 
  * 
  * ===== FLEX =====
- * - Flex direction;   - Align items
- * - Justify Content   - Order
- * 
  * docs: https://tailwindcss.com/docs/flex
+ * 
+ * - Flex direction;
+ *   docs: https://tailwindcss.com/docs/flex-direction
+ * 
+ * - Align items;
+ *   docs: https://tailwindcss.com/docs/align-items
+ * 
+ * - Justify Content
+ *   docs: https://tailwindcss.com/docs/justify-content
+ * 
+ * - Order
+ *   docs: https://tailwindcss.com/docs/order
+ * 
+ * 
+ * ==========================================================
+ * ========== CREAR UTILERIAS PERSONALIZADAS ==========
+ * en el taiwind.css
+ * 
+ * USANDO VALORES ARBITARIOS
+ * (estilos o valores que no viene por determinado)
+ * top: 117px => top-[117px]
+ * 
+ * docs: https://tailwindcss.com/docs/adding-custom-styles#using-arbitrary-values
+ * 
+ * 
+ * USING CSS AND @LAYER
+ * docs: https://tailwindcss.com/docs/adding-custom-styles#using-css-and-layer
+ * 
+ * 
+ * @variants => se usa para las versiones 2.2.16
+    docs: https://tailwindcss.com/docs/upgrade-guide#replace-variants-with-layer
+ * 
+ *
+ * ==========================================================
+ * ========== VARIANTES ==========
+ * 
+ * VARIANTE => Son las instrucciones que utilizamos
+ * para que lograr que nuestros diseños cambien dependiendo
+ * de las cirscunstancias
+ * 
+ * docs: https://tailwindcss.com/docs/hover-focus-and-other-states
+ * 
+ * 
+ *  - responsive => se ejecuta al cambio de resolución
+ *                  u orientación.
+ *  
+ *  - hover => se ejecuta cuando colocamos el cursor
+ *             encima o fuera de un elemento
+ * 
+ *  - focus => Se ejecuta cuando el elemento tiene el foco
+ *  
+ *  - disabled => se ejecuta cual el elemento es deshabilitado
+ * 
+ * 
+ * PSEUDO-CLASS => La palabra clave que se añade a la clase
+ *                 para logar el diseño deseado.
+ * 
+ * https://tailwindcss.com/docs/hover-focus-and-other-states#pseudo-classes
+ * 
+ *     - Responsive: sm, sd, lg, xl
+ *      docs: https://tailwindcss.com/docs/responsive-design
+ * 
+ *     - Hover: hover
+ *     - Focus: focus
+ *      docs: https://tailwindcss.com/docs/hover-focus-and-other-states#hover-focus-and-active
+ * 
+ *     - Disabled => disabled
+ *      docs: https://tailwindcss.com/docs/hover-focus-and-other-states#form-states
+ * 
+ * 
+ * REFERENCIAS DE MODIFICADORES Y PSEUDO-CLASS
+ * https://tailwindcss.com/docs/hover-focus-and-other-states#appendix
  * */
